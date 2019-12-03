913141bfc8f6e2cdaac5b91101791adfcdc7e4ea
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-12-03 23:07:06 +1100

    render to html with ejs

--- index.js
+++ index.js
@@ -39,9 +39,7 @@ const colors = {
     }
 };
 
-const GITHUB = {
-    userUrl: "https://api.github.com/users/",
-};
+const GITHUB_URL = "https://api.github.com/users/";
 
 //https://www.google.com/maps/search/?api=1&query=sydney%2caustralia
 const GOOGLE_URL = "https://www.google.com/maps/search/?api=1&query=";
@@ -49,9 +47,12 @@ const INQUIRER = require("inquirer");
 const AXIOS = require("axios");
 const FS = require("fs");
 const UTILS = require("util");
+const EJS = require("ejs");
 
 const readFile = UTILS.promisify(FS.readFile);
 const writeFile = UTILS.promisify(FS.writeFile);
+
+
 async function writeToFile(fileName, data) {
     try {
         await writeFile(fileName, data, "utf-8");
@@ -60,37 +61,21 @@ async function writeToFile(fileName, data) {
     }
 }
 
-const TEMPLATE_VARIABLES = {
-    headerBackground: "$[headerBackgroundColor]",
-    headerColor: "$[headerColor]",
-    wrapperBackground: "$[wrapperBackgroundColor]",
-    photo: "$[avatarUrl]",
-    name: "$[name]",
-    company: "$[company]",
-    location: "$[locationUrl]",
-    github: "$[githubUrl]",
-    blog: "$[blogUrl]",
-    nRepos: "$[nRepos]",
-    nFollowers: "$[nfollowers]",
-    nStars: "$[nStars]",
-    nFollowings: "$[nFollowings]",
-    isHidden: "$[isHidden]",
-};
-
 async function init() {
     try {
         const { backgroundColor:favColor, githubUsername } = await INQUIRER.prompt(questions);
-        const getGithubData = AXIOS.get(GITHUB.userUrl + githubUsername);
-        const getGitHubStars = AXIOS.get(GITHUB.userUrl + githubUsername+"/starred");
+        const userGithubUrl = GITHUB_URL + githubUsername;
+        const getGithubData = AXIOS.get(userGithubUrl);
+        const getGitHubStars = AXIOS.get(`${userGithubUrl}/starred`);
         const getTemplate = readFile("./template.html", "utf-8");
         const [
             {
                 data: {
                     avatar_url:avatarUrl,
-                    html_url: url,
+                    html_url: githubUrl,
                     name,
                     company,
-                    blog,
+                    blog: blogUrl,
                     location,
                     bio,
                     public_repos:repos,
@@ -103,41 +88,39 @@ async function init() {
             },
             template
         ] = await Promise.all([getGithubData, getGitHubStars,getTemplate]);
-        const locationUrl = GOOGLE_URL+location;
-        const {
-            wrapperBackground,
-            headerBackground,
-            headerColor,
-            photoBorderColor
-        } = colors[favColor];
-        let workplace, summary; 
-        let result = template.replace(/\$\[\bname\]/g,name);
-        result = result.replace(/\$\[wrapperBackgroundColor\]/g, wrapperBackground);
-        result = result.replace(/\$\[headerBackgroundColor\]/g, headerBackground);
-        result = result.replace(/\$\[headerColor\]/g, headerColor);
-        result = result.replace(/\$\[photoBorderColor\]/g, photoBorderColor);
-        result = result.replace(/\$\[avatarUrl\]/g, avatarUrl);
-        if(company){
-            workplace = `Currently @ ${company}`;
-        }else{
-            workplace = "";
-        }
-        result = result.replace(/\$\[company\]/g,workplace);
-        result = result.replace(/\$\[locationUrl\]/g,locationUrl);
-        result = result.replace(/\$\[githubUrl\]/g, url);
-        result = result.replace(/\$\[blogUrl\]/g,blog);
-        if(bio){
-            summary = bio;
-        }else{
-            summary = "";
-        }
-        result = result.replace(/\$\[bio\]/g,summary);
-        result = result.replace(/\$\[nRepos\]/g,repos);
-        result = result.replace(/\$\[nfollowers\]/g,followers);
-        result = result.replace(/\$\[nStars\]/g,starredRepos.length);
-        result = result.replace(/\$\[nFollowings\]/g,following);
+    let locationUrl = "";
+    if(location) {locationUrl = GOOGLE_URL+location;}
+    const {
+        wrapperBackground,
+        headerBackground,
+        headerColor,
+        photoBorderColor
+    } = colors[favColor];
 
 
+        const result = await EJS.render(
+            template, 
+            {
+                name,
+                wrapperBackground,
+                headerBackground,
+                headerColor,
+                photoBorderColor,
+                avatarUrl,
+                company,
+                locationUrl,
+                githubUrl,
+                blogUrl,
+                bio,
+                repos,
+                followers,
+                stars: starredRepos.length,
+                following
+            }, 
+            {
+                async: true
+            }
+        );
         writeToFile("index.html", result);
     } catch (err) {
         console.log(err);
